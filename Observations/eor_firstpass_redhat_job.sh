7b1d8801cb02a146864c348e90ebb3da2a4f54c5
wenyang li <wenyang_li@brown.edu>
2018-11-15 20:31:24 -0500

    add missing scripts

---
+++ Observations/eor_firstpass_redhat_job.sh
@@ -0,0 +1,65 @@
+#! /bin/bash
+#############################################################################
+#Runs one observation at a time on Slurm.  Second level program for 
+#running firstpass on the MIT machines. First level program is 
+#batch_firstpass.sh
+#############################################################################
+
+#SBATCH -J firstpass
+# #SBATCH --mail-type=ALL
+# #SBATCH --mail-user=adam_lanman@brown.edu
+
+# obs_id, outdir, version, and thresh and nslots expected to be passed from qsub call
+# in batch_firstpass.sh
+
+## Print all environmental variables
+#printenv
+
+#ls $outdir > /dev/null # ping the output directory so nfs automounts
+
+## TODO: check wedge stats and stop if above thresh
+#if [ "${thresh}" -gt "0" ]; then
+#    # check db for wedgie
+#    echo Checking wedgie
+#    export PGPASSWORD=BowTie	
+#    # first check that there is an entry
+#    exists=`psql -h eor-00 mwa_qc -U mwa -c "select count(*) from qs where obsid=${obs_id};" -t -A`
+#    if [ $exists -eq 1 ]; then
+#	under=`psql -h eor-00 mwa_qc -U mwa -c "select count(*) from qs where obsid=${obs_id} and window_x<${thresh} and window_y<${thresh};" -t -A`
+#	if [ $under -eq 0 ]; then
+#	    echo Obs $obs_id did not meet wedge threshold. Skipping!
+#	    status_file=${outdir}/fhd_${version}/metdata/${obs_id}_status.txt
+#	    touch $status_file
+#	    echo 'WEDGE_FLAGGED 1' > ${status_file}
+#	    echo 'COMPLETE 1' >> ${status_file}
+#	    exit 1
+#	else
+#	    echo Obs $obs_id met wedge criteria. Proceeding.
+#	fi
+#    else
+#	echo Did not find wedge stats for obs ${obs_id}. Proceeding.
+#    fi
+#fi
+
+#ls $outdir > /dev/null # ping the output directory so nfs automounts
+
+module load ghostscript
+module load git/2.10.2
+module load imagemagick/7.0.7
+module load idl
+shopt -s expand_aliases; source $IDL/envi53/bin/envi_setup.bash
+
+obsids=("$@")
+obs_id=${obsids[$SLURM_ARRAY_TASK_ID]}
+
+idl -IDL_DEVICE ps -quiet -IDL_CPU_TPOOL_NTHREADS $ncores -e eor_firstpass_versions -args $obs_id $outdir $version 
+
+if [ $? -eq 0 ]
+then
+    echo "Finished"
+    exit 0
+else
+    echo "Job Failed"
+    exit 1
+fi
+
---
+++ fhd_core/calibration/gain_cal_auto_divide.pro
@@ -0,0 +1,53 @@
+FUNCTION gain_cal_auto_divide,obs,cal,vis_auto=vis_auto,auto_tile_i=auto_tile_i
+
+dimension=obs.dimension
+elements=obs.elements
+n_pol=cal.n_pol ;do not use the cross-polarizations if they are present!
+n_freq=obs.n_freq
+n_tile=obs.n_tile
+i_comp=Complex(0,1)
+n_spectral=obs.degrid_spectral_terms
+
+frequency_array=(*obs.baseline_info).freq
+freq_delta=(frequency_array-obs.freq_center)/obs.freq_center
+n_tile_use=N_Elements(auto_tile_i)
+freq_bin_i=(*obs.baseline_info).fbin_i
+nfreq_bin=Max(freq_bin_i)+1
+
+freq_i_use=where((*obs.baseline_info).freq_use)
+freq_i_flag=where((*obs.baseline_info).freq_use EQ 0, n_freq_flag)
+IF n_freq_flag GT 0 THEN BEGIN
+    freq_flag=intarr(n_freq)
+    freq_flag[freq_i_use]=1
+    FOR freq_i=0,n_freq_flag-1 DO freq_flag[(freq_i_flag[freq_i]-1)>0:(freq_i_flag[freq_i]+1)<(n_freq-1)]=0
+    freq_i_use2=where(freq_flag)
+ENDIF ELSE freq_i_use2=freq_i_use
+
+
+auto_gain=Ptrarr(n_pol)
+FOR pol_i=0,n_pol-1 DO BEGIN
+    gain_arr=Complexarr(n_freq,n_tile)+1.
+    v0 = (*vis_auto[pol_i])[*,auto_tile_i[0]]
+    FOR freq_i=0L,n_freq-1 DO BEGIN
+        FOR tile_i=0,n_tile_use-1 DO BEGIN
+            gain_single=Sqrt((*vis_auto[pol_i])[freq_i,tile_i]*weight_invert(v0[freq_i]))
+            gain_arr[freq_i,auto_tile_i[tile_i]]=gain_single
+        ENDFOR
+    ENDFOR
+    auto_gain[pol_i]=Ptr_new(gain_arr)
+ENDFOR
+
+gain_cross=cal.gain
+FOR pol_i=0,n_pol-1 DO BEGIN
+    FOR tile_i_i=0L,n_tile_use-1 DO BEGIN
+        tile_i=auto_tile_i[tile_i_i]
+        gain_tile_i = (*gain_cross[pol_i])[*,tile_i]
+        gain_auto_single=Abs((*auto_gain[pol_i])[*,tile_i])
+        FOR freq_i=0L,n_freq-1 DO BEGIN
+            gain_tile_i[freq_i] /=  gain_auto_single[freq_i]
+        ENDFOR
+        (*cal.gain[pol_i])[*,tile_i] = gain_tile_i
+    ENDFOR
+ENDFOR
+RETURN,auto_gain
+END
